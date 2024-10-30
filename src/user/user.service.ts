a9fd4ef776380b94aa1a31a337c215b6985dab78
Llorens19 <llorenssorianodiego@gmail.com>
2024-10-30 17:44:08 +0100

    Realizar mejoras en el register y login ya funcionando

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,12 +16,11 @@ export const userRegisterService = async (request: FastifyRequest): Promise<IRes
 
     try {
         //!Cambiar en todos los servidores el error de la respuesta por duplicidad por el 500
-        await axios.post(`${process.env.SERVER_CLIENT}/register`, { user });
+        await axios.post(`${process.env.SERVER_CLIENT}/register`, { user, userType });
 
         switch (userType) {
             case 'client':
                 user_resp = await axios.post(`${process.env.SERVER_CLIENT}/user`, { user });
-                console.log("usurario cliente dentro del servicio", user_resp.data);
                 break;
             case 'company':
                 user_resp = await axios.post(`${process.env.SERVER_COMPANY}/user`, { user });
@@ -36,11 +35,11 @@ export const userRegisterService = async (request: FastifyRequest): Promise<IRes
         return resp(200, user_resp.data);
 
     } catch (error) {
+        console.log("error", error);
         if (axios.isAxiosError(error)) {
             let status = error.response?.status;
             const data = error.response?.data;
             const url = error.config?.url;
-            console.log("error", status);
             switch (status) {
                 case 500:
                     switch (url) {
@@ -49,7 +48,6 @@ export const userRegisterService = async (request: FastifyRequest): Promise<IRes
                         case `${process.env.SERVER_CLIENT}/user`:
                         case `${process.env.SERVER_COMPANY}/user`:
                         case `${process.env.SERVER_REGISTER}/user`:
-                            console.log("compensando");
                             axios.delete(`${process.env.SERVER_CLIENT}/user/${user.username}`);
                             return resp(status, { message: 'El correo o el usuraio ya existe.' });
 
@@ -74,23 +72,24 @@ export const userLoginService = async (request: FastifyRequest): Promise<IResp> 
     let user_resp = {} as AxiosResponse;
 
     try {
-        const resp_user_type = await axios.get(`${process.env.SERVER_CLIENT}/user/type/${user.email}`);
-        const user_type = resp_user_type.data.type;
+        const resp_user_type = await axios.post(`${process.env.SERVER_CLIENT}/user/type`, { user });
+        const user_type = resp_user_type.data.user.userType;
+        console.log("user_type", user_type);
 
         switch (user_type) {
-            case 'userClient':
+            case 'client':
                 user_resp = await axios.post(`${process.env.SERVER_CLIENT}/user/login`, { user });
                 break;
-            case 'userCompany':
+            case 'company':
                 user_resp = await axios.post(`${process.env.SERVER_COMPANY}/user/login`, { user });
                 break;
-            case 'userRecruiter':
+            case 'recruiter':
                 user_resp = await axios.post(`${process.env.SERVER_RECRUITER}/user/login`, { user });
                 break;
             default:
                 throw new Error('Tipo de usuario inválido');
         }
-        return resp(200, user_resp.data);
+        return resp(200, { user: user_resp.data, type: user_type });
 
     } catch (error) {
         return resp(500, { message: 'Ocurrió un error inesperado.' });
